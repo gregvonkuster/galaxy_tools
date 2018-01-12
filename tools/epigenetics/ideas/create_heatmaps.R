2dca8514bebe3fa2c1eb0d98be3d015020da41a6
greg <ghv2@psu.edu>
2018-01-12 14:57:32 -0500

    Fixes for IDEAS training mode

--- tools/epigenetics/ideas/create_heatmaps.R
+++ tools/epigenetics/ideas/create_heatmaps.R
@@ -13,22 +13,23 @@ parser <- OptionParser(usage="%prog [options] file", option_list=option_list);
 args <- parse_args(parser, positional_arguments=TRUE);
 opt <- args$options;
 
-heatmap_path <- paste(opt$script_dir, "create_heatmap.R", sep="/");
+heatmap_path = paste(opt$script_dir, "create_heatmap.R", sep="/");
 source(heatmap_path);
 
 if (opt$in_training_mode) {
-    ext <- ".para0";
-    para_files <- list.files(path=opt$input_dir, pattern="\\.para0$", full.names=TRUE);
+    ext = ".para0";
+    pattern = "\\.para0$";
 } else {
-    ext <- ".para";
-    para_files <- list.files(path=opt$input_dir, pattern="\\.para$", full.names=TRUE);
+    ext = ".para";
+    pattern = "\\.para$"
 }
+para_files = list.files(path=opt$input_dir, pattern=pattern, full.names=TRUE);
 for (i in 1:length(para_files)) {
-    para_file <- para_files[i];
-    para_file_base_name <- strsplit(para_file, split="/")[[1]][2];
-    output_file_base_name <- gsub(ext, "", para_file_base_name);
-    output_file_name <- paste(output_file_base_name, "state", i, "pdf", sep=".");
-    output_file_path <- paste(opt$output_dir, output_file_name, sep="/");
-    data_frame <- read.table(para_file, comment="!", header=T);
+    para_file = para_files[i];
+    para_file_base_name = strsplit(para_file, split="/")[[1]][2];
+    output_file_base_name = gsub(ext, "", para_file_base_name);
+    output_file_name = paste(output_file_base_name, "state", i, "pdf", sep=".");
+    output_file_path = paste(opt$output_dir, output_file_name, sep="/");
+    data_frame = read.table(para_file, comment="!", header=T);
     create_heatmap(data_frame, output_file_path);
 }
