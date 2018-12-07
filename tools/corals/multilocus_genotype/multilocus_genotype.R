b3e7a04f858b884faf410013db5d293c8a52674a
greg <ghv2@psu.edu>
2018-12-07 09:12:16 -0500

    More multilocus_genotype tool fixes

--- tools/corals/multilocus_genotype/multilocus_genotype.R
+++ tools/corals/multilocus_genotype/multilocus_genotype.R
@@ -11,6 +11,7 @@ suppressPackageStartupMessages(library("optparse"))
 suppressPackageStartupMessages(library("poppr"))
 suppressPackageStartupMessages(library("RColorBrewer"))
 suppressPackageStartupMessages(library("RPostgres"))
+suppressPackageStartupMessages(library("tidyr"))
 suppressPackageStartupMessages(library("vcfR"))
 suppressPackageStartupMessages(library("vegan"))
 
@@ -58,7 +59,6 @@ vcf <- read.vcfR(opt$input_vcf);
 # TODO: probably should not hard-code 2 cores.
 gl <- vcfR2genlight(vcf, n.cores=2);
 gind <- new("genind", (as.matrix(gl)));
-cat("made it past gind...\n");
 
 # Add population information to the genind object.
 poptab <- read.table(opt$input_pop_info, check.names=FALSE, header=F, na.strings=c("", "NA"), stringsAsFactors=FALSE, sep="\t");
@@ -67,16 +67,13 @@ gind@pop <- as.factor(poptab$region);
 
 # Convert genind object to a genclone object.
 obj2 <- as.genclone(gind);
-cat("made it past obj2...\n");
 
 # Calculate the bitwise distance between individuals.
 xdis <- bitwise.dist(obj2);
-cat("made it past xdis...\n");
 
 # Multilocus genotypes (threshold of 16%).
 mlg.filter(obj2, distance=xdis) <- 0.016;
 m <- mlg.table(obj2, background=TRUE, color=TRUE);
-cat("made it past m...\n");
 
 # Create table of MLGs.
 id <- mlg.id(obj2);
@@ -85,7 +82,6 @@ setnames(dt, c("id"), c("affy_id"));
 
 # Read user's Affymetrix 96 well plate csv file.
 pinfo <- read.table(opt$input_affy_metadata, header=FALSE, stringsAsFactors=FALSE, sep="\t");
-cat("made it past pinfo...\n");
 colnames(pinfo) <- c("date_entered_db", "user_specimen_id", "field_call", "bcoral_genet_id", "bsym_genet_id",
                      "reef", "region", "latitude", "longitude", "geographic_origin",
                      "sample_location", "latitude_outplant", "longitude_outplant", "depth", "dist_shore",
@@ -94,50 +90,40 @@ colnames(pinfo) <- c("date_entered_db", "user_specimen_id", "field_call", "bcora
                      "seq_facility", "array_version", "public", "public_after_date");
 pinfo$user_specimen_id <- as.character(pinfo$user_specimen_id);
 pinfo2 <- as.character(pinfo$user_specimen_id);
-cat("made it past pinfo2...\n");
 pi <- data.table(pinfo2);
 setnames(pi, c("pinfo2"), c("user_specimen_id"));
 
 # Connect to database.
 conn <- get_database_connection(opt$database_connection_string);
-cat("made it past conn...\n");
 
 # Import the sample table.
 mD <- tbl(conn, "sample");
-cat("made it past mD...\n");
 
 # Select user_specimen_id and mlg columns.
 smlg <- mD %>% select(user_specimen_id, coral_mlg_clonal_id, symbio_mlg_clonal_id, affy_id);
-cat("made it past smlg...\n");
 
 # Convert to dataframe.
 sm <- data.frame(smlg);
 sm[sm==""] <- NA;
-cat("made it past sm...\n");
 
 # Missing GT in samples submitted.
 gt <- extract.gt(vcf, element="GT", as.numeric=FALSE);
 myMiss <- apply(gt, MARGIN=2, function(x){ sum(is.na(x))});
 myMiss <- (myMiss / nrow(vcf)) * 100;
 miss <- data.frame(myMiss);
-cat("made it past miss...\n");
 
 # Convert missing data into data table.
-cat("Converting missing data into data table...\n");
 mi <-setDT(miss, keep.rownames=TRUE)[];
 setnames(mi, c("rn"), c("affy_id"));
 setnames(mi, c("myMiss"), c("percent_missing_data_coral"));
 # Round missing data to two digits.
 mi$percent_missing_data_coral <- round(mi$percent_missing_data_coral, digits=2);
-cat("made it past mi...\n");
 
 hets <- apply(gt, MARGIN=2, function(x) {sum(lengths(regmatches(x, gregexpr("0/1", x))))} );
 hets <- (hets / nrow(vcf)) * 100;
 ht <- data.frame(hets);
-cat("made it past ht...\n");
 
 # Convert heterozygosity data into data table.
-cat("Converting heterozygosity data into data table...\n");
 ht <-setDT(ht, keep.rownames=TRUE)[];
 setnames(ht, c("rn"), c("affy_id"));
 setnames(ht, c("hets"), c("percent_mixed_coral"));
@@ -147,10 +133,8 @@ ht$percent_mixed<-round(ht$percent_mixed, digits=2);
 refA <- apply(gt, MARGIN=2, function(x) {sum(lengths(regmatches(x, gregexpr("0/0", x))))} );
 refA <- (refA / nrow(vcf)) * 100;
 rA <- data.frame(refA);
-cat("made it past refA...\n");
 
 # Convert refA data into data.table.
-cat("Converting refA data into data table...\n");
 rA <-setDT(rA, keep.rownames=TRUE)[];
 setnames(rA, c("rn"), c("affy_id"));
 setnames(rA, c("refA"), c("percent_reference_coral"));
@@ -160,10 +144,8 @@ rA$percent_reference<-round(rA$percent_reference, digits=2);
 altB <- apply(gt, MARGIN=2, function(x) {sum(lengths(regmatches(x, gregexpr("1/1", x))))} );
 altB <- (altB / nrow(vcf)) * 100;
 aB <- data.frame(altB);
-cat("made it past aB...\n");
 
 # Convert altB data into data table.
-cat("Converting altB data into data table...\n");
 aB <-setDT(aB, keep.rownames=TRUE)[];
 setnames(aB, c("rn"), c("affy_id"));
 setnames(aB, c("altB"), c("percent_alternative_coral"));
@@ -171,12 +153,10 @@ setnames(aB, c("altB"), c("percent_alternative_coral"));
 aB$percent_alternative<-round(aB$percent_alternative, digits=2);
 
 #convert mlg id to data.table format
-cat("Converting MLG ID data into data table format...\n");
 dt <- data.table(id, keep.rownames=TRUE);
 setnames(dt, c("id"), c("affy_id"));
 
 # Transform.
-cat("Transforming...\n");
 df3 <- dt %>%
     group_by(row_number()) %>%
     dplyr::rename(group='row_number()') %>%
@@ -185,22 +165,18 @@ df3 <- dt %>%
     left_join(sm %>%
               select("affy_id","coral_mlg_clonal_id"),
               by='affy_id');
-cat("Made it past df3...\n");
 
 # If found in database, group members on previous mlg id.
 uniques <- unique(df3[c("group", "coral_mlg_clonal_id")]);
 uniques <- uniques[!is.na(uniques$coral_mlg_clonal_id),];
-cat("Made it past uniques\n");
 na.mlg <- which(is.na(df3$coral_mlg_clonal_id));
 na.group <- df3$group[na.mlg];
 df3$coral_mlg_clonal_id[na.mlg] <- uniques$coral_mlg_clonal_id[match(na.group, uniques$group)];
-cat("Made it past df3 again...\n");
 
 # Determine if the sample mlg matched previous genotyped sample.
 df4<- df3 %>%
     group_by(group) %>%
     mutate(DB_match = ifelse(is.na(coral_mlg_clonal_id),"no_match", "match"));
-cat("Made it past df4...\n");
 
 # Create new mlg id for samples that did not match those in the database.
 none <- unique(df4[c("group", "coral_mlg_clonal_id")]);
@@ -208,22 +184,18 @@ none <- none[is.na(none$coral_mlg_clonal_id),];
 na.mlg2 <- which(is.na(df4$coral_mlg_clonal_id));
 n.g <- df4$group[na.mlg2];
 ct <- length(unique(n.g));
-cat("Made it past ct...\n");
 
 # List of new group ids, the sequence starts at the number of
 # ids present in df4$coral_mlg_clonal_ids plus 1.  Not sure if
 # the df4 file contains all ids.  If it doesn't then look below
 # to change the seq() function. 
 n.g_ids <- sprintf("HG%04d", seq((sum(!is.na(unique(df4["coral_mlg_clonal_id"]))) + 1), by=1, length=ct));
-cat("Made it past n.g_ids...\n");
 # This is a key for pairing group with new ids.
 rat <- cbind(unique(n.g), n.g_ids);
-cat("Made it past rat...\n");
 # This for loop assigns the new id iteratively for all that have NA.
 for (i in 1:length(na.mlg2)) {
     df4$coral_mlg_clonal_id[na.mlg2[i]] <- n.g_ids[match(df4$group[na.mlg2[i]], unique(n.g))];
 }
-cat("Made it past for...\n");
 
 # subset poptab for all samples.
 subpop <- poptab[c(2, 3)];
@@ -252,10 +224,8 @@ report_user <- pi %>%
     mutate(coral_mlg_clonal_id = ifelse(is.na(coral_mlg_clonal_id), "failed", coral_mlg_clonal_id)) %>%
     ungroup() %>%
     select(-group);
-cat("Made it past report_user...\n");
 
 write.csv(report_user, file=opt$output_stag_db_report, quote=FALSE);
-cat("Made it past write.csv...\n");
 
 # Combine sample information for database.
 report_db <- pinfo %>%
@@ -264,7 +234,6 @@ report_db <- pinfo %>%
                "percent_missing_data_coral", "percent_mixed_coral", "percent_reference_coral",
                "percent_alternative_coral"),
         by='user_specimen_id');
-cat("Made it past report_db...\n");
 
 # Create vector indicating number of individuals desired
 # made from affy_id collumn of report_user data table.
@@ -301,5 +270,42 @@ pdf (file=file_path, width=10, height=7);
 par(mar = c(8, 4, 4, 2));
 x <- barplot(miss96, las=2, col=cols, ylim=c(0, 3), cex.axis=0.8, space=0.8, ylab="Missingness (%)", xaxt="n");
 text(cex=0.6, x=x-0.25, y=-.05, name96, xpd=TRUE, srt=60, adj=1);
+dev.off()
+
+# Generate 96 pie charts.  Make a table to subset the numerical
+# and user_specimen_id values out of report_user for the 96 pies
+# (user_specimen_id names will be used to label each pie).
+dt1 <- data.table(report_user);
+dt1 <- report_user[c(-2, -3, -4)];
+dt1 <- na.omit(dt1);
+# Translate to 96 columns and 5 rows.
+tdt1 <- t(dt1);
+# Make another data table and transpose it the same as dt1 to
+# just get numerics; these will feed into the creation of 96
+# vectors, "x" in the for loop below.
+dt2 <- data.table(report_user);
+dt2 <- report_user[c(-1, -2, -3, -4)];
+# Translate to 96 columns and 5 rows.
+tdt2 <- t(dt2);
+# Create 96 vectors
+x <- tdt2[1:96];
+tdt1_matrix <- as.matrix(tdt1[-1,]);
+mode(tdt1_matrix) <- "numeric";
+spy <- rowMeans(tdt1_matrix);
+dev.new(width=10, height=7);
+file_path = get_file_path("percent_breakdown.pdf");
+pdf(file=file_path, width=10, height=7);
+# Average pie of all samples.
+labels <- paste(c("missing data", "mixed", "reference", "alternative"), " (", round(spy, 1), "%)", sep="");
+col <- c("GREY", "#006DDB", "#24FF24", "#920000");
+main <- "Average breakdown of SNP assignments across all samples";
+pie(spy, labels=labels, radius=0.60, col=col, main=main, cex.main=.75);
+par(mfrow=c(3, 2));
+for (i in 1:96) {
+    labels <- paste(labels, " (", round(tdt1_matrix[,i], 1), "%)", sep="");
+    col <- c("GREY", "#006DDB", "#24FF24", "#920000");
+    main <- paste("Breakdown of SNP assignments for", tdt1[1, i]);
+    pie(tdt1_matrix[,i], labels=labels, radius=0.90, col=col, main=main, cex.main=.85, cex=0.75);
+}
 dev.off()
 
