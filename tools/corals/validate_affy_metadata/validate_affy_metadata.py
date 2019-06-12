4e9efe1f2b3efc7f5d4e12efa9b613af7ea0eab4
greg <ghv2@psu.edu>
2019-06-12 11:21:13 -0400

    Add 2 more columns to the affy_metadata

--- tools/corals/validate_affy_metadata/validate_affy_metadata.py
+++ tools/corals/validate_affy_metadata/validate_affy_metadata.py
@@ -71,19 +71,21 @@ with open(args.input, "r") as ih:
         if i == 0:
             # Skip the header.
             continue
+        # Keep 1-based line value for error messages.
+        line_no = i + 1
         line = line.rstrip("\r\n")
         if i > 97:
             accumulated_msgs = add_error_msg(accumulated_msgs, "The input file contains more than 97 lines (must be 1 header line and no more than 96 data lines).")
             stop_error(accumulated_msgs)
         items = line.split("\t")
-        if len(items) != 30:
-            accumulated_msgs = add_error_msg(accumulated_msgs, "Line %d contains %s columns, (must be 30)." % (i, len(items)))
+        if len(items) != 32:
+            accumulated_msgs = add_error_msg(accumulated_msgs, "Line %d contains %s columns, (must be 32)." % (line_no, len(items)))
             stop_error(accumulated_msgs)
         # Required and validated.
         # Required.
         user_specimen_id = items[0]
         if len(user_specimen_id) == 0:
-            accumulated_msgs = empty_value(i, "user_specimen_id", accumulated_msgs)
+            accumulated_msgs = empty_value(line_no, "user_specimen_id", accumulated_msgs)
         # Optional.
         field_call = items[1]
         # Optional.
@@ -93,17 +95,17 @@ with open(args.input, "r") as ih:
         # Required.
         reef = items[4]
         if len(reef) == 0:
-            accumulated_msgs = empty_value(i, "reef", accumulated_msgs)
+            accumulated_msgs = empty_value(line_no, "reef", accumulated_msgs)
         # Required.
         region = items[5]
         if len(region) == 0:
-            accumulated_msgs = empty_value(i, "region", accumulated_msgs)
+            accumulated_msgs = empty_value(line_no, "region", accumulated_msgs)
         # Required and validated.
         latitude = items[6]
-        accumulated_msgs = validate_decimal(i, latitude, "latitude", accumulated_msgs)
+        accumulated_msgs = validate_decimal(line_no, latitude, "latitude", accumulated_msgs)
         # Required and validated.
         longitude = items[7]
-        accumulated_msgs = validate_decimal(i, longitude, "longitude", accumulated_msgs)
+        accumulated_msgs = validate_decimal(line_no, longitude, "longitude", accumulated_msgs)
         # Optional.
         geographic_origin = items[8]
         # Optional.
@@ -123,47 +125,55 @@ with open(args.input, "r") as ih:
         # Required.
         collector_last_name = items[16]
         if len(collector_last_name) == 0:
-            accumulated_msgs = empty_value(i, "collector_last_name", accumulated_msgs)
+            accumulated_msgs = empty_value(line_no, "collector_last_name", accumulated_msgs)
         # Required.
         collector_first_name = items[17]
         if len(collector_first_name) == 0:
-            accumulated_msgs = empty_value(i, "collector_first_name", accumulated_msgs)
+            accumulated_msgs = empty_value(line_no, "collector_first_name", accumulated_msgs)
         # Required.
         org = items[18]
         if len(org) == 0:
-            accumulated_msgs = empty_value(i, "org", accumulated_msgs)
+            accumulated_msgs = empty_value(line_no, "org", accumulated_msgs)
         # Required and validated.
         collection_date = items[19]
-        accumulated_msgs = validate_date_string(i, collection_date, "collection_date", accumulated_msgs)
+        accumulated_msgs = validate_date_string(line_no, collection_date, "collection_date", accumulated_msgs)
         # Required and validated.
         contact_email = items[20]
-        accumulated_msgs = validate_email(i, contact_email, accumulated_msgs)
+        accumulated_msgs = validate_email(line_no, contact_email, accumulated_msgs)
         # Required.
         seq_facility = items[21]
         if len(seq_facility) == 0:
-            accumulated_msgs = empty_value(i, "seq_facility", accumulated_msgs)
+            accumulated_msgs = empty_value(line_no, "seq_facility", accumulated_msgs)
         # Optional.
         array_version = items[22]
         # Optional.
         public = string_as_boolean_string(items[23])
         # Optional.
         public_after_date = items[24]
-        accumulated_msga = validate_date_string(i, public_after_date, "public_after_date", accumulated_msgs)
+        accumulated_msga = validate_date_string(line_no, public_after_date, "public_after_date", accumulated_msgs)
         # Required and validated.
         sperm_motility = items[25]
-        accumulated_msgs = validate_decimal(i, sperm_motility, "sperm_motility", accumulated_msgs)
+        accumulated_msgs = validate_decimal(line_no, sperm_motility, "sperm_motility", accumulated_msgs)
         # Required and validated.
         healing_time = items[26]
-        accumulated_msgs = validate_decimal(i, healing_time, "healing_time", accumulated_msgs)
+        accumulated_msgs = validate_decimal(line_no, healing_time, "healing_time", accumulated_msgs)
         # Optional.
         dna_extraction_method = items[27]
         # Optional.
         dna_concentration = items[28]
         # If dna_concentration has a value, then it must be decimal.
         if len(dna_concentration) > 0:
-            accumulated_msgs = validate_decimal(i, dna_concentration, "dna_concentration", accumulated_msgs)
+            accumulated_msgs = validate_decimal(line_no, dna_concentration, "dna_concentration", accumulated_msgs)
         # Optional.
         registry_id = items[29]
+        # Required.
+        result_folder_name = items[30]
+        if len(result_folder_name) == 0:
+            accumulated_msgs = empty_value(line_no, "result_folder_name", accumulated_msgs)
+        # Required.
+        plate_barcode = items[31]
+        if len(plate_barcode) == 0:
+            accumulated_msgs = empty_value(line_no, "plate_barcode", accumulated_msgs)
 
 
 if len(accumulated_msgs) > 0:
