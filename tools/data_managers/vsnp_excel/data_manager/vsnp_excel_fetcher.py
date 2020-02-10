3a85d0fa29a31597974882f4c76d5a64c353f215
greg <ghv2@psu.edu>
2020-02-10 08:03:47 -0500

    Fixes for vsnp_excel data manager

--- tools/data_managers/vsnp_excel/data_manager/vsnp_excel_fetcher.py
+++ tools/data_managers/vsnp_excel/data_manager/vsnp_excel_fetcher.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import sys
 try:
     # For Python 3.0 and later
     from urllib.request import Request, urlopen
@@ -9,8 +10,8 @@ except ImportError:
     from urllib2 import Request, urlopen
 
 
-def url_download(url, target_directory):
-    file_path = os.path.abspath(os.path.join(target_directory, os.path.basename(url)))
+def url_download(url, workdir):
+    file_path = os.path.abspath(os.path.join(workdir, os.path.basename(url)))
     src = None
     dst = None
     try:
@@ -23,29 +24,30 @@ def url_download(url, target_directory):
                     dst.write(chunk)
                 else:
                     break
+    except Exception as e:
+        sys.exit(str(e))
     finally:
         if src:
             src.close()
     return file_path
 
 
-def download(dbkey, display_name, url, out_file):
+def download(dbkey, name, url, out_file):
 
     with open(out_file) as fh:
         params = json.loads(fh.read())
 
-    work_directory = params['output_data'][0]['extra_files_path']
-    target_directory = os.path.abspath(os.path.join(work_directory, "vsnp", dbkey))
-    os.mkdir(target_directory)
-    file_path = url_download(url, target_directory)
-    entry_name = "%s" % os.path.basename(file_path)
+    workdir = params['output_data'][0]['extra_files_path']
+    os.makedirs(workdir)
+    file_path = url_download(url, workdir)
+    entry_name = os.path.basename(file_path)
 
     data_manager_json = {"data_tables": {}}
     data_manager_entry = {}
     data_manager_entry['value'] = dbkey
     data_manager_entry['name'] = entry_name
     data_manager_entry['path'] = file_path
-    data_manager_entry['description'] = "Excel file for %s" % display_name
+    data_manager_entry['description'] = "Excel file for %s" % name
     data_manager_json["data_tables"]["vsnp_excel"] = data_manager_entry
 
     with open(out_file, 'w') as fh:
@@ -55,10 +57,10 @@ def download(dbkey, display_name, url, out_file):
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--dbkey', dest='dbkey', help='Genome reference dbkey')
-parser.add_argument('--display_name', dest='display_name', help='Reference display name')
+parser.add_argument('--name', dest='name', help='Reference display name')
 parser.add_argument('--url', dest='url', help='URL to download Excel file')
 parser.add_argument('--out_file', dest='out_file', help='JSON output file')
 
 args = parser.parse_args()
 
-download(args.dbkey, args.display_name, args.url, args.out_file)
+download(args.dbkey, args.name, args.url, args.out_file)
