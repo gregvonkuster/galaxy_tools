8f6f58fb6b450dec77932f7b7b96b59ed98ba7af
greg <ghv2@psu.edu>
2020-02-11 13:59:46 -0500

    Use better file diesciptior s and improve metrics output for vsnp_add_zero_coverage

--- tools/sequence_analysis/vsnp/vsnp_add_zero_coverage/vsnp_add_zero_coverage.py
+++ tools/sequence_analysis/vsnp/vsnp_add_zero_coverage/vsnp_add_zero_coverage.py
@@ -3,6 +3,7 @@
 import argparse
 import shutil
 import re
+import os
 import pandas
 import pysam
 from numpy import mean
@@ -36,7 +37,8 @@ for record in SeqIO.parse(args.reference, "fasta"):
     total_len = len(record.seq)
     for pos in list(range(1, total_len + 1)):
         zero_dict["%s-%s" % (str(chrom), str(pos))] = 0
-# Convert it to a data frame with depth_x andf depth_y columns - index is NaN.
+# Convert it to a data frame with depth_x
+# and depth_y columns - index is NaN.
 zero_df = pandas.DataFrame.from_dict(zero_dict, orient='index', columns=["depth"])
 coverage_df = zero_df.merge(coverage_df, left_index=True, right_index=True, how='outer')
 # depth_x "0" column no longer needed.
@@ -46,7 +48,7 @@ coverage_df = coverage_df.rename(columns={'depth_y': 'depth'})
 coverage_df = coverage_df.fillna(0)
 coverage_df['depth'] = coverage_df['depth'].apply(int)
 total_length = len(coverage_df)
-ave_coverage = coverage_df['depth'].mean()
+average_coverage = coverage_df['depth'].mean()
 zero_df = coverage_df[coverage_df['depth'] == 0]
 total_zero_coverage = len(zero_df)
 total_coverage = total_length - total_zero_coverage
@@ -55,10 +57,6 @@ genome_coverage = "{:.2%}".format(total_coverage / total_length)
 column_names = ["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", "Sample"]
 vcf_df = pandas.read_csv(args.vcf_input, sep='\t', header=None, names=column_names, comment='#')
 good_snp_count = len(vcf_df[(vcf_df['ALT'].str.len() == 1) & (vcf_df['REF'].str.len() == 1) & (vcf_df['QUAL'] > 150)])
-with open(args.output_metrics, "w") as fh:
-    fh.write("Number of good SNPs: %d\n" % good_snp_count)
-    fh.write("Average coverage: %4f\n" % ave_coverage)
-    fh.write("Genome coverage: %s\n" % genome_coverage)
 if total_zero_coverage > 0:
     header_file = HEADERFILE
     with open(header_file, 'w') as outfile:
@@ -87,6 +85,14 @@ if total_zero_coverage > 0:
         for cf in [HEADERFILE, BODYFILE]:
             with open(cf, "r") as infile:
                 for line in infile:
-                   outfile.write("%s" % line)
+                    outfile.write("%s" % line)
 else:
     shutil.copyfile(args.vcf_input, args.output_vcf)
+
+metrics_columns = ["File", "Number of Good SNPs", "Average Coverage", "Genome Coverage"]
+bam_metrics = [os.path.basename(args.bam_input), "", "%4f" % average_coverage, genome_coverage]
+vcf_metrics = [os.path.basename(args.vcf_input), str(good_snp_count), "", ""]
+with open(args.output_metrics, "w") as fh:
+    fh.write("# %s\n" % "\t".join(metrics_columns))
+    fh.write("%s\n" % "\t".join(bam_metrics))
+    fh.write("%s\n" % "\t".join(vcf_metrics))
