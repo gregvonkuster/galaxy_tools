5f2f5bb2ee8a83f54d09866f42acedeb565909d9
greg <ghv2@psu.edu>
2020-02-12 10:04:15 -0500

    Enhance vsnp_statistics to merge all stats into one output

--- tools/sequence_analysis/vsnp/vsnp_statistics/vsnp_statistics.py
+++ tools/sequence_analysis/vsnp/vsnp_statistics/vsnp_statistics.py
@@ -9,6 +9,7 @@ import pandas
 
 QUALITYKEY = {'!':'0', '"':'1', '#':'2', '$':'3', '%':'4', '&':'5', "'":'6', '(':'7', ')':'8', '*':'9', '+':'10', ',':'11', '-':'12', '.':'13', '/':'14', '0':'15', '1':'16', '2':'17', '3':'18', '4':'19', '5':'20', '6':'21', '7':'22', '8':'23', '9':'24', ':':'25', ';':'26', '<':'27', '=':'28', '>':'29', '?':'30', '@':'31', 'A':'32', 'B':'33', 'C':'34', 'D':'35', 'E':'36', 'F':'37', 'G':'38', 'H':'39', 'I':'40', 'J':'41', 'K':'42', 'L':'43', 'M':'44', 'N':'45', 'O':'46', 'P':'47', 'Q':'48', 'R':'49', 'S':'50', 'T':'51', 'U':'52', 'V':'53', 'W':'54', 'X':'55', 'Y':'56', 'Z':'57', '_':'1', ']':'1', '[':'1', '\\':'1', '\n':'1', '`':'1', 'a':'1', 'b':'1', 'c':'1', 'd':'1', 'e':'1', 'f':'1', 'g':'1', 'h':'1', 'i':'1', 'j':'1', 'k':'1', 'l':'1', 'm':'1', 'n':'1', 'o':'1', 'p':'1', 'q':'1', 'r':'1', 's':'1', 't':'1', 'u':'1', 'v':'1', 'w':'1', 'x':'1', 'y':'1', 'z':'1', ' ':'1'}
 READCOLUMNS = ['Sample', 'Reference', 'Fastq File', 'Size', 'Total Reads', 'Mean Read Length', 'Mean Read Quality', 'Reads Passing Q30']
+SEP = "\t"
 
 
 def nice_size(size):
@@ -32,31 +33,39 @@ def nice_size(size):
     return '??? bytes'
 
 
-def output_read_stats(gzipped, fastq_file, fh, sampling_number=10000, output_header=False):
-    row_items = []
-    try:
-        # Illumina read file names are something like:
-        # 13-1941-6_S4_L001_R1_600000_fastq_gz
-        sample = fastq_file.split("_")[0]
-    except Exception:
-        sample = ""
-    # Sample
-    row_items.append(sample)
-    # TODO: handle reference instead of setting to n/a.
-    row_items.append("n/a")
-    # Read
-    row_items.append(os.path.basename(fastq_file))
-    # File Size
-    row_items.append(nice_size(os.path.getsize(fastq_file)))
-    header = None
-    sep = '^'
-    if gzipped:
-        df = pandas.read_csv(gzip.open(fastq_file, "r"), header=header, sep=sep)
+def output_read_stats(gzipped, fastq_file, ofh, sampling_number=10000, output_sample=False):
+    # Output a 2-column file where column 1 is
+    # the labels and column 2 is the values.
+    if output_sample:
+        # The Sample and Reference columns should be
+        # output only once, so this block handles
+        # paired reads, where the columns are not
+        # output for Read2.
+        try:
+            # Illumina read file names are something like:
+            # 13-1941-6_S4_L001_R1_600000_fastq_gz
+            sample = fastq_file.split("_")[0]
+        except Exception:
+            sample = ""
+        # Sample
+        ofh.write("Sample%s%s\n" % (SEP, sample))
+        # TODO: handle reference instead of setting to n/a.
+        ofh.write("Reference%s%s\n" % (SEP, "n/a"))
+        read = "Read1"
     else:
-        df = pandas.read_csv(open(fastq_file, "r"), header=header, sep=sep)
+        read = "Read2"
+    # Read
+    ofh.write("%s File%s%s\n" % (read, SEP, os.path.basename(fastq_file)))
+    # File Size
+    ofh.write("%s File Size%s%s\n" % (read, SEP, nice_size(os.path.getsize(fastq_file))))
+    if gzipped:
+        df = pandas.read_csv(gzip.open(fastq_file, "r"), header=None, sep="^")
+    else:
+        df = pandas.read_csv(open(fastq_file, "r"), header=None, sep="^")
     total_read_count = int(len(df.index)/4)
-    # Fastq Total Reads
-    row_items.append(str(total_read_count))
+    # Readx Total Reads
+    ofh.write("%s Total Reads%s%s\n" % (read, SEP, total_read_count))
+    # Mean Read Length
     sampling_size = int(sampling_number)
     if sampling_size > total_read_count:
         sampling_size = total_read_count
@@ -69,18 +78,13 @@ def output_read_stats(gzipped, fastq_file, fh, sampling_number=10000, output_hea
             base_qualities.append(int(QUALITYKEY[base]))
         dict_mean[index] = numpy.mean(base_qualities)
         list_length.append(len(row.array[0]))
-    # Mean Read Length
-    row_items.append("%.1f" % numpy.mean(list_length))
-    df_mean = pandas.DataFrame.from_dict(dict_mean, orient='index', columns=['ave'])
+    ofh.write("%s Mean Read Length%s%s\n" % (read, SEP, "%.1f" % numpy.mean(list_length)))
     # Mean Read Quality
-    row_items.append("%.1f" % df_mean['ave'].mean())
-    reads_gt_q30 = len(df_mean[df_mean['ave'] >= 30])
+    df_mean = pandas.DataFrame.from_dict(dict_mean, orient='index', columns=['ave'])
+    ofh.write("%s Mean Read Quality%s%s\n" % (read, SEP, "%.1f" % df_mean['ave'].mean()))
     # Reads Passing Q30
-    row_items.append(f'{reads_gt_q30/sampling_size:0.1%}')
-    # Output read statistics.
-    if output_header:
-        fh.write("# %s\n" % "\t".join(READCOLUMNS))
-    fh.write("%s\n" % "\t".join(row_items))
+    reads_gt_q30 = len(df_mean[df_mean['ave'] >= 30])
+    ofh.write("%s Reads Passing Q30%s%s\n" % (read, SEP, f'{reads_gt_q30/sampling_size:0.1%}'))
     return total_read_count
 
 
@@ -89,13 +93,41 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-r1', '--read1', action='store', dest='read1', help='Required: single read')
 parser.add_argument('-r2', '--read2', action='store', dest='read2', required=False, default=None, help='Optional: paired read')
 parser.add_argument('-gz', '--gzipped', action='store', dest='gzipped', help='Input files are gzipped')
+parser.add_argument('-si', '--samtools_idxstats', action='store', dest='samtools_idxstats', help='Output of samtools_idxstats')
 parser.add_argument('-of', '--output', action='store', dest='output', help='Output statisticsfile')
+parser.add_argument('-va', '--vsnp_azc', action='store', dest='vsnp_azc', help='Output of vsnp_add_zero_coverage')
 
 args = parser.parse_args()
 
 total_reads = 0
 
-with open(args.output, "w") as fh:
-    total_reads += output_read_stats(args.gzipped, args.read1, fh, output_header=True)
+with open(args.output, "w") as ofh:
+    total_reads += output_read_stats(args.gzipped, args.read1, ofh, output_sample=True)
     if args.read2 is not None:
-        total_reads += output_read_stats(args.gzipped, args.read2, fh)
+        total_reads += output_read_stats(args.gzipped, args.read2, ofh)
+    # Total Reads
+    ofh.write("Total Reads%s%d\n" % (SEP, total_reads))
+    with open(args.samtools_idxstats, "r") as ifh:
+        for i, line in enumerate(ifh):
+            items = line.split("\t")
+            if i == 0:
+                # NC_002945.4 4349904 213570 4047
+                ofh.write("All Mapped Reads%s%s\n" % (SEP, items[2]))
+            elif i == 1:
+                # * 0 0 82774
+                ofh.write("Unmapped Reads%s%s\n" % (SEP, items[3]))
+    with open(args.vsnp_azc, "r") as ifh:
+        for i, line in enumerate(ifh):
+            if i == 0:
+                # Skip comments.
+                continue
+            items = line.split("\t")
+            if i == 1:
+                # MarkDuplicates 10.338671 98.74%
+                ofh.write("Average Depth of Coverage%s%s\n" % (SEP, items[2]))
+                ofh.write("Reference with Coverage%s%s\n" % (SEP, items[3]))
+            elif i == 2:
+                # VCFfilter 611
+                ofh.write("Good SNP Count%s%s\n" % (SEP, items[1]))
+            # TODO: handle Unmapped Assembled Contigs which requires
+            # Abyss tool execution (see line ~196 in vsnp_alignment_vcf.py).
