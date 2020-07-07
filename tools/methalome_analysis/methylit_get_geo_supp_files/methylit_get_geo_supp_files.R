30f0939ab647eb33041658f0827540fc917bd067
Greg Von Kuster <ghv2@psu.edu>
2020-07-07 12:48:36 -0400

    Fixes for methylit_get_geo_supp_files

--- tools/methalome_analysis/methylit_get_geo_supp_files/methylit_get_geo_supp_files.R
+++ tools/methalome_analysis/methylit_get_geo_supp_files/methylit_get_geo_supp_files.R
@@ -6,7 +6,7 @@ suppressPackageStartupMessages(library("RCurl"))
 
 option_list <- list(
     make_option(c("--geo"), action="store", dest="geo", help="Comma-separated list of GEO accession numbers"),
-    make_option(c("--pattern"), action="store", dest="pattern", default=NULL, help="pattern for GEO accession files"),
+    make_option(c("--pattern"), action="store", dest="pattern", default=NULL, help="pattern for GEO accession files")
 )
 
 parser <- OptionParser(usage="%prog [options] file", option_list=option_list);
@@ -15,11 +15,17 @@ opt <- args$options;
 
 # Convert GEO accession numbers into a character vector.
 geo_str <- as.character(opt$geo);
+cat("\ngeo_str:\n");
+geo_str
 geo_list <- strsplit(geo_str, ",")[[1]];
+cat("\ngeo_list:\n");
+geo_list
 geo <- c(unlist(geo_list, use.names=FALSE));
+cat("\ngeo:\n");
+geo
 
 
-df <- getGEOSuppFiles(GEO=c("GSM1279513"), makeDirectory=FALSE, baseDir='output_data_dir', pattern=opt$pattern, verbose=TRUE);
+df <- getGEOSuppFiles(GEO=geo, makeDirectory=FALSE, baseDir='output_data_dir', pattern=opt$pattern, verbose=TRUE);
 cat("\ndf:\n");
 df
 cat("\ntypeof(df):\n");
