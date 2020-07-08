5e764f7198d42634812fc517aa103c0835790442
Greg Von Kuster <ghv2@psu.edu>
2020-07-08 10:30:07 -0400

    Cleanup for methylit_get_geo_supp_files tool

--- tools/methalome_analysis/methylit_get_geo_supp_files/methylit_get_geo_supp_files.R
+++ tools/methalome_analysis/methylit_get_geo_supp_files/methylit_get_geo_supp_files.R
@@ -6,6 +6,7 @@ suppressPackageStartupMessages(library("RCurl"))
 
 option_list <- list(
     make_option(c("--geo"), action="store", dest="geo", help="Comma-separated list of GEO accession numbers"),
+    make_option(c("--output_dir"), action="store", dest="output_dir", help="Output directory for downloading files"),
     make_option(c("--pattern"), action="store", dest="pattern", default=NULL, help="pattern for GEO accession files")
 )
 
@@ -15,19 +16,9 @@ opt <- args$options;
 
 # Convert GEO accession numbers into a character vector.
 geo_str <- as.character(opt$geo);
-cat("\ngeo_str:\n");
-geo_str
 geo_list <- strsplit(geo_str, ",")[[1]];
-cat("\ngeo_list:\n");
-geo_list
 geo <- c(unlist(geo_list, use.names=FALSE));
-cat("\ngeo:\n");
-geo
 
-
-df <- getGEOSuppFiles(GEO=geo, makeDirectory=FALSE, baseDir='output_data_dir', pattern=opt$pattern, verbose=TRUE);
-cat("\ndf:\n");
-df
-cat("\ntypeof(df):\n");
-typeof(df)
+# Download the files to the output directory.
+df <- getGEOSuppFiles(GEO=geo, makeDirectory=FALSE, baseDir=opt$output_dir, pattern=opt$pattern, verbose=TRUE);
 
