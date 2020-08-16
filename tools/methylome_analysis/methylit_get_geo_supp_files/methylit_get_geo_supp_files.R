ad4254ed57e91b16c89d89ae989b3fb40de9423d
Greg Von Kuster <ghv2@psu.edu>
2020-08-16 16:24:06 -0400

    methylit_get_geo_supp_files cleanup

--- tools/methylome_analysis/methylit_get_geo_supp_files/methylit_get_geo_supp_files.R
+++ tools/methylome_analysis/methylit_get_geo_supp_files/methylit_get_geo_supp_files.R
@@ -10,17 +10,19 @@ option_list <- list(
     make_option(c("--geo"), action="store", dest="geo", help="Comma-separated list of GEO accession numbers"),
     make_option(c("--output"), action="store", dest="output", defaul=NULL, help="Single output file"),
     make_option(c("--output_dir"), action="store", dest="output_dir", help="Output directory for downloading files"),
-    make_option(c("--pattern"), action="store", dest="pattern", default=NULL, help="pattern for GEO accession files")
+    make_option(c("--pattern"), action="store", dest="pattern", default=NULL, help="pattern for GEO accession files"),
+    make_option(c("--script_dir"), action="store", dest="script_dir", help="R script source directory")
 )
 
 parser <- OptionParser(usage="%prog [options] file", option_list=option_list);
 args <- parse_args(parser, positional_arguments=TRUE);
 opt <- args$options;
 
-# Convert GEO accession numbers into a character vector.
-geo_str <- as.character(opt$geo);
-geo_list <- strsplit(geo_str, ",")[[1]];
-geo <- c(unlist(geo_list, use.names=FALSE));
+# Import the shared utility functions.
+utils_path <- paste(opt$script_dir, "utils.R", sep="/");
+source(utils_path);
+
+geo <- string_to_charactor_vector(opt$geo);
 
 # Download the file(s) to the output directory.
 df <- getGEOSuppFiles(GEO=geo, makeDirectory=FALSE, baseDir=opt$output_dir, pattern=opt$pattern, verbose=TRUE);
