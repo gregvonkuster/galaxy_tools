405c6e291604b145ab940b5159b4b8beee4a1a37
greg <ghv2@psu.edu>
2023-03-17 15:39:51 -0400

    Fix typo in pima_report

--- tools/pima/pima_report/pima_report.py
+++ tools/pima/pima_report/pima_report.py
@@ -300,7 +300,7 @@ class PimaReport:
             "ONT FASTQ",
             self.wordwrap_markdown(self.ont_raw_fastq),
             "Illumina FASTQ",
-            "N/A"
+            "N/A",
             "Assembly",
             self.wordwrap_markdown(self.assembly_name),
             "Reference",
