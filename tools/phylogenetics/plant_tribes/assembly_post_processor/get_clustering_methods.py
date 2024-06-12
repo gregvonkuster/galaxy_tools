eee4ddedf1e35f2658a1e94bd5493144bda3fa85
greg <ghv2@psu.edu>
2024-06-12 13:03:46 -0400

    Modernize PlantTribes assembly post processor

--- tools/phylogenetics/plant_tribes/assembly_post_processor/get_clustering_methods.py
+++ tools/phylogenetics/plant_tribes/assembly_post_processor/get_clustering_methods.py
@@ -1,1 +1,11 @@
-../get_clustering_methods.py
+import os
+
+
+def get_clustering_method_options(file_path):
+    options = []
+    if not os.path.isdir(file_path):
+        return options
+    methods_dir = os.path.join(file_path, "alns")
+    for i, dir_name in enumerate(os.listdir(methods_dir)):
+        options.append((dir_name, dir_name, i == 0))
+    return options
